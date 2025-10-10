49c7343a8c30e049c3e5f9df5bcda60e42a70685
yameenist <160256133+yameenist@users.noreply.github.com>
2025-10-11 03:19:04 +0500

    app.py

--- app.py
+++ app.py
@@ -432,7 +432,7 @@ list_and_display_files(announcement_path, "Announcements")  # ✅ New section
 
 # ✅ Function to determine shift based on date
 def get_shift(duty_time, selected_date):
-    start_date = datetime.date(2025, 6, 1)
+    start_date = datetime.date(2025, 5, 10)
     shift_intervals = [
         (start_date + datetime.timedelta(days=7 * i), start_date + datetime.timedelta(days=7 * (i + 1) - 1))
         for i in range(4)
@@ -561,6 +561,7 @@ if selected_name and selected_name.strip():
 # ✅ Footer
 st.markdown("<hr style='border: 1px solid #ddd;'>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; font-size: 14px; color: #555;'>🚀 This application is created by <b>Muhammad Yameen Saleem</b></p>", unsafe_allow_html=True)
+
 
 
 
